a9a86eb1eda1a9f890100b60be48d0b0e7a0598d
patlewig <patlewig@hotmail.com>
2023-04-10 21:39:49 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Evaluating the utility of a high throughput thiol-containing fluorescent probe to screen for reactivity: A case study with the Tox21 library. [D[D[D[D[D[',
+    author='patlewig',
+    license='MIT',
+)
